fc132c17bbaab78cc24e9dfb72044173d7ae06cd
FrancoB4 <francobonfigliovazquez@gmail.com>
2023-01-05 16:54:07 -0300

    Allowed origin and methods

--- src/main/java/com/final_project/portfolio/PortfolioApplication.java
+++ src/main/java/com/final_project/portfolio/PortfolioApplication.java
@@ -2,12 +2,25 @@ package com.final_project.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PortfolioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE");
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200",
+						"my-personal-portfolio-3f485.web.app", "my-personal-portfolio-3f485.firebaseapp.com");
+			}
+		};
 	}
 }
--- src/main/java/com/final_project/portfolio/model/User.java
+++ src/main/java/com/final_project/portfolio/model/User.java
@@ -15,5 +15,4 @@ public class User {
     private long id;
 
     private String username, password;
-
 }
